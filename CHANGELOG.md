3539e354dfcc2003d1215be9f540c8fa9526de40
Enes BULUT <bulut0258@gmail.com>
2023-07-20 15:19:58 +0300

    stable versionand

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.0
+
+* Migrated to stable versionand a bug is fixed.
+
 ## 0.0.9
 
 * First initial release.
--- lib/joystick.dart
+++ lib/joystick.dart
@@ -46,30 +46,39 @@ class _JoystickState extends State<Joystick> {
     return Stack(
       children: <Widget>[
         Positioned(
-          bottom: _y,
-          right: _x,
-          child: Container(
-              //container alanının yukseklik ve genişlik değerleri
-              height: widget.size,
-              width: widget.size,
-              decoration: BoxDecoration(
-                  color:
-                      widget.backgroundColor.withOpacity(widget.opacity ?? 1) ??
+            bottom: _y,
+            right: _x,
+            child: GestureDetector(
+              onLongPress: (widget.isDraggable == false)
+                  ? null
+                  : () {
+                      setState(() {
+                        _x = 0;
+                        _y = 0;
+                      });
+                    },
+              child: Container(
+                  //container alanının yukseklik ve genişlik değerleri
+                  height: widget.size,
+                  width: widget.size,
+                  decoration: BoxDecoration(
+                      color: widget.backgroundColor
+                              .withOpacity(widget.opacity ?? 1) ??
                           Colors.orange,
-                  shape: BoxShape.circle),
-              child: Column(children: [
-                //Up
-                Expanded(
-                    flex: 1,
-                    child: Row(
-                      children: <Widget>[
-                        Expanded(
-                          child: SizedBox(),
-                        ),
-                        Expanded(
-                          //Üst ok bölümü
-                          child:
-                              (widget.joystickMode == JoystickMode.horizontal)
+                      shape: BoxShape.circle),
+                  child: Column(children: [
+                    //Up
+                    Expanded(
+                        flex: 1,
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: SizedBox(),
+                            ),
+                            Expanded(
+                              //Üst ok bölümü
+                              child: (widget.joystickMode ==
+                                      JoystickMode.horizontal)
                                   ? SizedBox()
                                   : IconButton(
                                       padding: EdgeInsets.all(
@@ -85,86 +94,88 @@ class _JoystickState extends State<Joystick> {
                                           widget.onPressed(Directions.up);
                                       },
                                     ),
-                        ),
-                        Expanded(
-                          child: SizedBox(),
-                        )
-                      ],
-                    )),
-                //Middle
-                Expanded(
-                    flex: 1,
-                    child: Row(
-                      children: <Widget>[
-                        Expanded(
-                          //sol buton alanı
-                          child: (widget.joystickMode == JoystickMode.vertical)
-                              ? SizedBox()
-                              : IconButton(
-                                  padding: EdgeInsets.all(
-                                      0), //kenar alan yapılandırılması-Paddin alanı
-                                  icon: Icon(Icons.keyboard_arrow_left,
-                                      color: (widget.iconColor == null)
-                                          ? Colors.blue
-                                          : widget.iconColor),
-                                  onPressed: () {
-                                    if (widget.onLeftPressed != null)
-                                      widget.onLeftPressed();
-                                    if (widget.onPressed != null)
-                                      widget.onPressed(Directions.left);
-                                  },
-                                ),
-                        ),
-                        Expanded(
-                          //Sürükle- bırak bölümü
-                          child: GestureDetector(
-                            child: Icon(Icons.drag_handle,
-                                color: (widget.iconColor == null)
-                                    ? Colors.blue
-                                    : widget.iconColor),
-                            onPanUpdate: (details) {
-                              if (widget.isDraggable == true) {
-                                setState(() {
-                                  _x -= details.delta.dx;
-                                  _y -= details.delta.dy;
-                                });
-                              }
-                            },
-                          ),
-                        ),
-                        //Sağ ok alanı
-                        Expanded(
-                          child: (widget.joystickMode == JoystickMode.vertical)
-                              ? SizedBox()
-                              : IconButton(
-                                  padding: EdgeInsets.all(
-                                      0), //kenar alan yapılandırılması-Paddin alanı
-                                  icon: Icon(Icons.keyboard_arrow_right,
-                                      color: (widget.iconColor == null)
-                                          ? Colors.blue
-                                          : widget.iconColor),
-                                  onPressed: () {
-                                    if (widget.onRightPressed != null)
-                                      widget.onRightPressed();
-                                    if (widget.onPressed != null)
-                                      widget.onPressed(Directions.right);
-                                  },
-                                ),
-                        )
-                      ],
-                    )),
-                //Down
-                Expanded(
-                    flex: 1,
-                    child: Row(
-                      children: <Widget>[
-                        Expanded(
-                          child: SizedBox(),
-                        ),
-                        Expanded(
-                          //Alt ok alanı
-                          child:
-                              (widget.joystickMode == JoystickMode.horizontal)
+                            ),
+                            Expanded(
+                              child: SizedBox(),
+                            )
+                          ],
+                        )),
+                    //Middle
+                    Expanded(
+                        flex: 1,
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(
+                              //sol buton alanı
+                              child:
+                                  (widget.joystickMode == JoystickMode.vertical)
+                                      ? SizedBox()
+                                      : IconButton(
+                                          padding: EdgeInsets.all(
+                                              0), //kenar alan yapılandırılması-Paddin alanı
+                                          icon: Icon(Icons.keyboard_arrow_left,
+                                              color: (widget.iconColor == null)
+                                                  ? Colors.blue
+                                                  : widget.iconColor),
+                                          onPressed: () {
+                                            if (widget.onLeftPressed != null)
+                                              widget.onLeftPressed();
+                                            if (widget.onPressed != null)
+                                              widget.onPressed(Directions.left);
+                                          },
+                                        ),
+                            ),
+                            Expanded(
+                              //Sürükle- bırak bölümü
+                              child: GestureDetector(
+                                child: Icon(Icons.drag_handle,
+                                    color: (widget.iconColor == null)
+                                        ? Colors.blue
+                                        : widget.iconColor),
+                                onPanUpdate: (details) {
+                                  if (widget.isDraggable == true) {
+                                    setState(() {
+                                      _x -= details.delta.dx;
+                                      _y -= details.delta.dy;
+                                    });
+                                  }
+                                },
+                              ),
+                            ),
+                            //Sağ ok alanı
+                            Expanded(
+                              child: (widget.joystickMode ==
+                                      JoystickMode.vertical)
+                                  ? SizedBox()
+                                  : IconButton(
+                                      padding: EdgeInsets.all(
+                                          0), //kenar alan yapılandırılması-Paddin alanı
+                                      icon: Icon(Icons.keyboard_arrow_right,
+                                          color: (widget.iconColor == null)
+                                              ? Colors.blue
+                                              : widget.iconColor),
+                                      onPressed: () {
+                                        if (widget.onRightPressed != null)
+                                          widget.onRightPressed();
+                                        if (widget.onPressed != null)
+                                          widget.onPressed(Directions.right);
+                                      },
+                                    ),
+                            )
+                          ],
+                        )),
+                    //Down
+                    Expanded(
+                        flex: 1,
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: SizedBox(),
+                            ),
+                            Expanded(
+                              //Alt ok alanı
+                              child: (widget.joystickMode ==
+                                      JoystickMode.horizontal)
                                   ? SizedBox()
                                   : IconButton(
                                       padding: EdgeInsets.all(
@@ -180,14 +191,14 @@ class _JoystickState extends State<Joystick> {
                                           widget.onPressed(Directions.down);
                                       },
                                     ),
-                        ),
-                        Expanded(
-                          child: SizedBox(),
-                        )
-                      ],
-                    )),
-              ])),
-        )
+                            ),
+                            Expanded(
+                              child: SizedBox(),
+                            )
+                          ],
+                        )),
+                  ])),
+            ))
       ],
     );
   }
